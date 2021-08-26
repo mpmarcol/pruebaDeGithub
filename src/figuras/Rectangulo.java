7b4beea8bab7498275cb9c15c0ecad4d5934fc75
Miguel <mpmarcol@gmail.com>
2021-08-26 19:36:18 -0300

    Se agrega el metodo para calculo de perimetro

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -5,8 +5,12 @@ public class Rectangulo {
 	private double ancho;
 	private double alto;
 
-	public Rectangulo (double ancho, double alto) {
+	public Rectangulo(double ancho, double alto) {
 		this.ancho = ancho;
 		this.alto = alto;
+	}
+
+	public double getPerimetro() {
+		return this.alto * 2 + this.ancho * 2;
 	}
 }
